75cafc0e1af97207c289ecdaab4aec73f9435ab2
meier.i@husky.neu.edu <43007421+meieri@users.noreply.github.com>
2020-06-16 17:03:26 -0400

    setting up routing

---
+++ src/Components/GameOfGames.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter } from 'react-router-dom'
+
+export default class GameOfGames extends React.Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+        
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
