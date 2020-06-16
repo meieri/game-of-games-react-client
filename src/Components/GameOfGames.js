ba9329ff760406e89ad44af2ea92e1475e9afcf2
meier.i@husky.neu.edu <43007421+meieri@users.noreply.github.com>
2020-06-16 18:02:43 -0400

    Wokring on landing page

--- src/Components/GameOfGames.js
+++ src/Components/GameOfGames.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+import LandingComponent from "./LandingComponent";
+import './GameOfGames.css'
 
 export default class GameOfGames extends React.Component {
   render() {
     return (
       <div>
+        <div className='navbar'>
+          <a className='navbar-brand gameHeading'>Game Of Games</a>
+          <div>
+            <a className='mr-3'>Log In</a>
+            <a className='mr-3'>Sign Up</a>
+          </div>
+        </div>
         <BrowserRouter>
-        
+          <Route
+            path='/'
+            exact={true}
+            component={LandingComponent}/>
         </BrowserRouter>
       </div>
+
     );
   }
 }
---
+++ src/Components/LandingComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './LandingComponent.css'
+
+export default class LandingComponent extends React.Component {
+  render() {
+    return(
+      <h1>Landing Component</h1>
+    )
+  }
+}
