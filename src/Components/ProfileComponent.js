571c6860f41e092983c3728d3f13194e5527d6ea
meier.i@husky.neu.edu <43007421+meieri@users.noreply.github.com>
2020-06-17 20:34:24 -0400

    sort of built profile componenet, working on create game component'

--- src/Components/ProfileComponent.js
+++ src/Components/ProfileComponent.js
@@ -1,9 +1,15 @@
 import React from "react";
+import './ProfileComponent.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEdit} from '@fortawesome/free-solid-svg-icons'
+import {Link} from "react-router-dom";
 
 export default class ProfileComponent extends React.Component {
   state = {
     username: '',
-    password: ''
+    password: '',
+    editingUsername: false,
+    editingPassword: false
   }
 
   componentDidMount() {
@@ -20,7 +26,7 @@ export default class ProfileComponent extends React.Component {
         this.props.history.push('/')
       })
       .then(user => {
-        if(user)
+        if (user)
           this.setState({
             username: user.username,
             password: user.password
@@ -36,14 +42,97 @@ export default class ProfileComponent extends React.Component {
       .then(this.props.history.push('/'))
 
   render() {
-    return(
-      <div>
-        <h1>Profile Screen</h1>
-        <h2>{this.state.username}</h2>
-        <h2>{this.state.password}</h2>
-        <button
-          onClick={() => this.logout()}
-        >Log Out</button>
+    return (
+      <div className='container'>
+        <div className='d-flex justify-content-center'>
+          <h1>Welcome, {this.state.username}</h1>
+        </div>
+        <div className='container mt-3'>
+          <div className='row'>
+            <div className='col-5 profileInfo'>
+              <h1>User Info</h1>
+              <form className='mt-3 '>
+
+                <div className="form-group row">
+                  <label htmlFor="usernameField" className="col-form-label ml-3">Username:</label>
+                  <fieldset disabled={!this.state.editingUsername} className='ml-3'>
+                    <input
+                      type="text"
+                      id="usernameField"
+                      className="form-control"
+                      onChange={(e) => this.setState({username: e.target.value})}
+                      placeholder={this.state.username}/>
+                  </fieldset>
+                  {
+                    !this.state.editingUsername &&
+                    <button
+                      className='btn btn-outline-light ml-3'
+                      type='button'
+                      onClick={() => this.setState({editingUsername: true})}>
+                      <FontAwesomeIcon icon={faEdit}/>
+                    </button>
+                  }
+                  {
+                    this.state.editingUsername &&
+                    <button
+                      className='btn btn-outline-light ml-3'
+                      type='button'
+                      onClick={() => this.setState({editingUsername: false})}>
+                      Submit
+                    </button>
+                  }
+                </div>
+
+                <div className="form-group row">
+                  <label htmlFor="passwordField" className="col-form-label ml-3 mr-1">Password:</label>
+                  <fieldset disabled={!this.state.editingPassword} className='ml-3'>
+                    <input
+                      type="password"
+                      id="passwordField"
+                      className="form-control"
+                      onChange={(e) => this.setState({password: e.target.value})}
+                      placeholder='&#9679;&#9679;&#9679;&#9679;&#9679;'/>
+                  </fieldset>
+                  {
+                    !this.state.editingPassword &&
+                    <button
+                      className='btn btn-outline-light ml-3'
+                      type='button'
+                      onClick={() => this.setState({editingPassword: true})}>
+                      <FontAwesomeIcon icon={faEdit}/>
+                    </button>
+                  }
+                  {
+                    this.state.editingPassword &&
+                        <button
+                          className='btn btn-outline-light ml-3'
+                          type='button'
+                          onClick={() => this.setState({editingPassword: false})}>
+                          Submit
+                        </button>
+                  }
+                </div>
+
+                <button className='btn btn-outline-light mb-3' onClick={() => this.logout()}>Log Out</button>
+
+              </form>
+            </div>
+
+            <div className='col-7'>
+              <button
+                onClick={() => this.props.history.push('/create')}
+                className='btn btn-outline-light btn-lg'>Create New Game</button>
+              <h1>Past Games</h1>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-5'>
+            </div>
+            <div className='col-7'>
+              <h1>Spotify</h1>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
