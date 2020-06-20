f480c63242d9f56b9763822f246c2d6667150a75
meier.i@husky.neu.edu <43007421+meieri@users.noreply.github.com>
2020-06-20 18:29:39 -0400

    now on to end game page

--- src/Components/PlayGameComponent.js
+++ src/Components/PlayGameComponent.js
@@ -11,7 +11,9 @@ class PlayGameComponent extends React.Component {
     showAnswer: false,
     team1Points: 0,
     team2Points: 0,
-    team1turn: true
+    team1turn: true,
+    playedQuestions: [],
+    youWannaBeDone: false
   }
 
   // TODO remap 'cc' to 'dd O' in vim
@@ -26,39 +28,70 @@ class PlayGameComponent extends React.Component {
     }
   }
 
+  endGame = () =>
+    console.log("gameEnd")
+
   render() {
     return (
       <div>
         {
           this.state.playingQuestion === '' &&
-          <div>
-            <div className='container-fluid game-board mt-3'>
-              <div className='row d-flex justify-content-center'>
-                {this.state.game &&
-                this.state.game.categories.map(category =>
-                  <div
-                    key={category.id}
-                    className='btn col game-col mx-2'>
-                    <div className='row game-card-cat d-flex justify-content-center align-content-center'>
-                      {category.name}
-                    </div>
-                    {category.questions.map(question =>
-                      <div key={question.id}
-                           className='row game-card d-flex justify-content-center align-content-center mt-2'
-                           onClick={() => this.setState({playingQuestion: question})}>
-                        <div>{question.value}</div>
-                      </div>
-                    )}
+          <div className='container-fluid game-board mt-3'>
+            <div className='row d-flex justify-content-center'>
+              {this.state.game &&
+              this.state.game.categories.map(category =>
+                <div
+                  key={category.id}
+                  className='btn col game-col mx-2'>
+                  <div className='row game-card-cat d-flex justify-content-center align-content-center'>
+                    {category.name}
                   </div>
-                )}
-              </div>
+                  {category.questions.map(question =>
+                    <div>
+                      {
+                        !this.state.playedQuestions.includes(question.id) &&
+                        <div key={question.id}
+                             className='row game-card d-flex justify-content-center align-content-center mt-2'
+                             onClick={() => this.setState({playingQuestion: question})}>
+                          <div>{question.value}</div>
+                        </div>
+                      }
+                      {
+                        this.state.playedQuestions.includes(question.id) &&
+                        <div key={question.id}
+                             className='row game-card-played d-flex justify-content-center align-content-center mt-2'>
+                          <div>{question.value}</div>
+                        </div>
+                      }
+                    </div>
+                  )}
+                </div>
+              )}
             </div>
             <div className='container-fluid d-inline-flex justify-content-between mt-3 team-container'>
-              <div className={this.state.team1Turn ? '' : 'my-turn'}>
+              <div className={this.state.team1turn ? 'my-turn' : ''}>
                 <div className='py-1 px-2'>Team 1: {this.state.team1Points}</div>
               </div>
+              <div>
+                {!this.state.youWannaBeDone &&
+                <button
+                  onClick={() => this.setState({youWannaBeDone: true})}
+                  className='btn btn-outline-light'>End Game</button>
+                }
+                {this.state.youWannaBeDone &&
+                <div>
+                  Are you sure
+                  <button
+                    onClick={() => this.endGame()}
+                    className='ml-3 btn btn-outline-light'>Yes</button>
+                  <button
+                    onClick={() => this.setState({youWannaBeDone: false})}
+                    className='ml-3 btn btn-outline-light'>No</button>
+                </div>
+                }
+              </div>
               <div
-                className={this.state.team1Turn ? 'my-turn' : ''}>
+                className={this.state.team1turn ? '' : 'my-turn'}>
                 <div className='py-1 px-2'>Team 2: {this.state.team2Points}</div>
               </div>
             </div>
@@ -79,8 +112,7 @@ class PlayGameComponent extends React.Component {
               {this.state.showAnswer &&
               <div>
                 <div
-                  className='question centered'
-                  onClick={() => this.setState({showAnswer: false, playingQuestion: ''})}>
+                  className='question centered'>
                   {this.state.playingQuestion.answer}
                 </div>
 
@@ -89,11 +121,16 @@ class PlayGameComponent extends React.Component {
                     <div
                       onClick={
                         () => this.setState(prevState => ({
+                          ...prevState,
                           team1turn: true,
-                          team1Points: prevState.team1Points + this.state.playingQuestion.value
+                          team1Points: prevState.team1Points + this.state.playingQuestion.value,
+                          showAnswer: false,
+                          playingQuestion: '',
+                          playedQuestions: [...prevState.playedQuestions, this.state.playingQuestion.id]
                         }))
                       }
-                      className='py-1 px-2 select-winner'>Team 1</div>
+                      className='py-1 px-2 select-winner'>Team 1
+                    </div>
                   </div>
 
                   <h1>Select Winner</h1>
@@ -102,21 +139,21 @@ class PlayGameComponent extends React.Component {
                     onClick={
                       () => this.setState(prevState => ({
                         team1turn: false,
-                        team2Points: prevState.team2Points + this.state.playingQuestion.value
+                        team2Points: prevState.team2Points + this.state.playingQuestion.value,
+                        showAnswer: false,
+                        playingQuestion: '',
+                        playedQuestions: [...prevState.playedQuestions, this.state.playingQuestion.id]
                       }))
                     }>
                     <div className='py-1 px-2 select-winner'>Team 2</div>
                   </div>
                 </div>
-
               </div>
               }
             </div>
 
           </div>
         }
-
-
       </div>
     )
   }
